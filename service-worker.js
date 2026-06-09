b6fe37068e5cc04c28df28bbd680fa0ff5b80640
anacaks05-sketch <ana.c.aks05@gmail.com>
2026-06-09 08:33:44 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,12 @@
-const CACHE_NAME = "prado-sports-v15";
-const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "prado-sports-v16-fix";
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(["./index.html?v=16fix","./manifest.json","./icon-192.png","./icon-512.png"])));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
 });
 self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
