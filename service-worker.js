db27ae3f88df72e8385e5e2f887f377c7dbe32dd
anacaks05-sketch <ana.c.aks05@gmail.com>
2026-06-09 09:23:57 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE_NAME = "prado-sports-v17-clean";
-const FILES = ["./index.html?v=17","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install", e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});
-self.addEventListener("activate", e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim()});
-self.addEventListener("fetch", e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+const CACHE_NAME="prado-sports-v18";self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["./index.html?v=18","./manifest.json","./icon-192.png","./icon-512.png"])))});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim()});self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
