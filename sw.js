c0c978ff4c7aef1e8af81837974eeb7d3bd67cd0
anacaks05-sketch <ana.c.aks05@gmail.com>
2026-06-10 11:55:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prado-ia-v12-scan-mesma-aba';
+const CACHE_NAME = 'prado-ia-v13-scan-mesma-aba';
 const ASSETS = ['./','./index.html','./styles.css','./app.js?v=12','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
